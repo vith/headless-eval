27fd28fa49a3f3ea872a1a867af86bf9cdf4b595
Jason Papakostas <vithos@gmail.com>
2020-05-01 11:19:31 +0200

    add tests of cli

---
+++ test/cli.ts
@@ -0,0 +1,56 @@
+import path from 'path'
+import { promisify } from 'util'
+import pkgDir from 'pkg-dir'
+
+import avaTest, { ExecutionContext, TestInterface } from 'ava'
+import execa from 'execa'
+import npm from 'npm'
+
+import { harvardSentences, sentencesQuerySelectorStringified } from './fixtures/data'
+import startServer, { PortContext } from './fixtures/testServer'
+
+type CliContext = {
+	tsNodePath: string,
+	headlessEvalCliPath: string,
+}
+
+type PortAndCliContext = PortContext & CliContext
+
+const test = avaTest as TestInterface<PortAndCliContext>
+
+test.before(startServer)
+
+test.before(async function findPaths(t: ExecutionContext<CliContext>) {
+	await promisify(npm.load)()
+	t.context.tsNodePath = path.join(npm.bin, 'ts-node')
+
+	const headlessEvalPkgDir = await pkgDir()
+	if (!headlessEvalPkgDir)
+		throw new Error("Couldn't find headless-eval package directory")
+	t.context.headlessEvalCliPath = path.join(headlessEvalPkgDir, 'src', 'cli.ts')
+})
+
+test('extraction via cli', async (t: ExecutionContext<PortAndCliContext>) => {
+	t.plan(1)
+
+	const { stdout } = await execa(t.context.tsNodePath, [
+		t.context.headlessEvalCliPath,
+		`http://localhost:${t.context.port}/sentences`,
+		sentencesQuerySelectorStringified,
+	])
+
+	t.is(stdout, harvardSentences.join('\n'), 'extracted sentences')
+})
+
+test('json extraction via cli', async (t: ExecutionContext<PortAndCliContext>) => {
+	t.plan(1)
+
+	const { stdout } = await execa(t.context.tsNodePath, [
+		t.context.headlessEvalCliPath,
+		'-j',
+		`http://localhost:${t.context.port}/sentences`,
+		sentencesQuerySelectorStringified,
+	])
+
+	t.deepEqual(JSON.parse(stdout), harvardSentences, 'extracted sentences as json')
+})
---
+++ test/fixtures/data.ts
@@ -0,0 +1,15 @@
+export const harvardSentences = [
+	'Oak is strong and also gives shade.',
+	'Cats and dogs each hate the other.',
+	'The pipe began to rust while new.',
+]
+
+export const sentencesQuerySelector = () =>
+[...document.querySelectorAll("ul#harvard-sentences > li")]
+	.map(li => {
+		if (!(li instanceof HTMLLIElement))
+			throw new TypeError
+		return li.innerText
+	})
+
+export const sentencesQuerySelectorStringified = `(${sentencesQuerySelector})()`
